d0b8f2a8e0ea44191bdb2c2ce002e7523092bfac
DgteMendez <pilotomendez123@gmail.com>
2026-05-11 22:34:27 -0400

    configurando el postgresssss

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ require('dotenv').config();
 
 const app = express();
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Requerido para Render
+  }
+});
+
+// Ruta para probar la base de datos
+app.get('/db-test', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ msg: "Conectado a la DB con éxito", time: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
